435874ce7487a81ecde087e695ee6bdbced23822
Anderson Lima <anderson.lima873@matera.com>
2025-10-19 12:36:31 -0300

    improving app with AI

--- cdk.out/asset.5e46bb14f288e85dd5b012b1df844950de70ef1f01fee58ae9e27a255eb27aef/testapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import hello_world, health_check
-
-
-urlpatterns = [
-    path('', hello_world),
-    path('health/', health_check)
-]
--- cdk.out/asset.b4d3232da0f2fbad5a6239385f735e7180eb4f569b7a4c3370b9635f58c43154/testapp/management/commands/__init__.py
+++
@@ -1,1 +0,0 @@
-# noqa: D104 - namespace package for Django management commands.
